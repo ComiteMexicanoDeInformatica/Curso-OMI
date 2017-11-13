9d264881a583e9b04b545089db3d68d32fb101cd
Angel Chi Poot <angel.linux91@gmail.com>
2017-11-13 13:48:28 -0600

    Cambios Solucion
    Cambios para estar de acuerdo con el documento de explicacion de la solucion

--- mente torcida/solution/solution.cpp
+++ mente torcida/solution/solution.cpp
@@ -1,4 +1,6 @@
-#include <cstdio>
+#include <iostream>
+
+using namespace std;
 
 int min(int a, int b) {
 	//Toma dos numeros y regresa el mas pequeno
@@ -18,28 +20,28 @@ int max(int a, int b) {
 }
 
 int main() {
-	int left_1, bottom_1, right_1, top_1;
-	int left_2, bottom_2, right_2, top_2;
-	int left_intr, bottom_intr, right_intr, top_intr;
+	int izq_1, abajo_1, der_1, arriba_1;
+	int izq_2, abajo_2, der_2, arriba_2;
+	int izq_intr, abajo_intr, der_intr, arriba_intr;
 
 	int area_1, area_2, area_intr, area_total;
 
 	/*Lee la entrada*/
-	scanf("%d %d %d %d",&left_1,&bottom_1,&right_1,&top_1);
-	scanf("%d %d %d %d",&left_2,&bottom_2,&right_2,&top_2);
+	cin >> izq_1 >> abajo_1 >> der_1 >> arriba_1;
+	cin >> izq_2 >> abajo_2 >> der_2 >> arriba_2;
 
 	/*Calcula la interseccion de las coordenadas*/
-	left_intr = max(left_1,left_2);
-	bottom_intr = max(bottom_1,bottom_2);
-	right_intr = min(right_1,right_2);
-	top_intr = min(top_1,top_2);
+	izq_intr = max(izq_1,izq_2);
+	abajo_intr = max(abajo_1,abajo_2);
+	der_intr = min(der_1,der_2);
+	arriba_intr = min(arriba_1,arriba_2);
 
 	/*Calcula las areas de los rectangulos y la interseccion.*/
-	area_1 = (right_1-left_1) * (top_1-bottom_1);
-	area_2 = (right_2-left_2) * (top_2-bottom_2);
-	if (left_intr < right_intr && bottom_intr < top_intr) {
+	area_1 = (der_1-izq_1) * (arriba_1-abajo_1);
+	area_2 = (der_2-izq_2) * (arriba_2-abajo_2);
+	if (izq_intr < der_intr && abajo_intr < arriba_intr) {
 		//La interseccion tiene sentido
-		area_intr = (right_intr-left_intr) * (top_intr-bottom_intr);
+		area_intr = (der_intr-izq_intr) * (arriba_intr-abajo_intr);
 	} else {
 		//There doesn't seem to be an intersection
 		area_intr = 0;
@@ -47,5 +49,5 @@ int main() {
 	area_total = area_1 + area_2 - area_intr;
 
 	/*Salida.*/
-	printf("%d\n",area_total);
+	cout << area_total << endl;
 }
