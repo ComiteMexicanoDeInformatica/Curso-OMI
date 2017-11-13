045c9b474ef059b5247290c41894f54bf43ef1b2
AburaCaxal <rafael_ac@hotmail.com>
2017-11-13 12:36:13 -0600

    Solución de Sequia

---
+++ sequia/solution/solution.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int n, c, lluvia, total;
+
+int main()
+{
+   	cin>>n>>c;
+	for (int d=1; d<=n; d++) {
+		cin>>lluvia;
+		total+=lluvia;
+		if (total >= c) {
+			cout<<d; //respuesta de días en que se llenó
+			break;	//no se requiere continuar
+		}
+	}
+    return 0;
+}
