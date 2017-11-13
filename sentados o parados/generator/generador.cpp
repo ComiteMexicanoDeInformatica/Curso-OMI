8ee89dca778b6132aeae76a68647925f6d91b760
lacj24 <lacj.luis@gmail.com>
2017-11-13 12:08:32 -0600

    caso y generador
    Se corrigió el caso 0.in.
    Se agregó el generador de casos para este problema

---
+++ sentados o parados/generator/generador.cpp
@@ -0,0 +1,79 @@
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <random>
+
+using namespace std;
+void solucion(string entrada,string salida);
+int main()
+{
+
+    /**Arreglo para definir los limites de cada caso**/
+    int casos[10] = {100,100,500,500,1000,1000,5000,5000,10000,10000};
+
+    /**generador de aleatorios**/
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    /**Ciclo para generar los casos en archivos .in**/
+    for (int n=0; n<10; ++n)
+    {
+        ofstream in;
+        stringstream ss;
+        ss << n+1;
+        string str = ss.str();
+        in.open(str+".in");
+        /** inicia tu codigo para generar el caso**/
+        if(n%2==0) //en casos pares no hay sobreventa de tickets
+        {
+            /**la función dis establece el límite inferior y superior del random**/
+            std::uniform_int_distribution<> dis(1 , sqrt(casos[n]));
+            int r = dis(gen);
+            int s = dis(gen);
+            in << r <<" "<<s<<endl<<casos[n];
+        }
+        else      //en casos impares hay sobreventa de tickets
+        {
+            std::uniform_int_distribution<> dis(casos[n]/2+1 , casos[n]);
+            int r = dis(gen);
+            int s = dis(gen);
+            int sobreventa=1000000000-r*s;
+            in << r <<" "<<s<<endl<<sobreventa;
+        }
+        /**termina tu código para generar el caso**/
+
+        in.close();
+        solucion(str+".in",str+".out");
+
+    }
+    return 0;
+}
+/** función que recibe el nombre del archivo .in y el archivo .out**/
+void solucion(string entrada,string salida)
+{
+    /**inicia la redirección de cin y cout. NO BORRAR**/
+    std::ifstream in(entrada);
+    std::streambuf *cinbuf = std::cin.rdbuf();
+    std::cin.rdbuf(in.rdbuf());
+
+    std::ofstream out(salida);
+    std::streambuf *coutbuf = std::cout.rdbuf();
+    std::cout.rdbuf(out.rdbuf());
+    /**termina la redirección de cin y cout**/
+
+    /**la solución del problema**/
+    int r,s,b,ns;
+    cin>>r>>s>>b;
+    ns=r*s;
+    if(ns<=b)
+    {
+        cout<<ns<<" "<<b-ns;
+    }
+    else
+    {
+        cout<<b<<" "<<0;
+    }
+    cout<<endl;
+}
