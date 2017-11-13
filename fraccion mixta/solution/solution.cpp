6e78fb3760b7a0e7dbe3bcab968abe699ce0fee0
ErickB <erick.jassi.bs@gmail.com>
2017-11-12 21:50:34 -0600

    solución Mixed Fraction

---
+++ fraccion mixta/solution/solution.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n, d, a, b;
+    cin >> n >> d;
+
+    a = n / d;
+    b = n % d;
+
+    if (b != 0){
+        cout << a << ' ' << b << '/' << d << endl;
+    } else {
+        cout << a << endl;
+    }
+}
