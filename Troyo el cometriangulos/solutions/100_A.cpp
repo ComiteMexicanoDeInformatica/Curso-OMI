57af4d53a968e8b4115c058f0145c19c431600c8
Briaares <diego.gonzalez@cimat.mx>
2017-11-14 17:50:00 -0600

    Troyín

---
+++ Troyo el cometriangulos/solutions/100_A.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+ll n;
+	int main (){
+		ios_base::sync_with_stdio(0);
+		cin.tie(0);
+		cin>>n;
+		cout << (ll)((n*(n+1))/2LL)<<"\n";
+	}
