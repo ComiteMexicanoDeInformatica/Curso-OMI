23e05285e18678d207f66ca33722b8ebf72acc32
Briaares <diego.gonzalez@cimat.mx>
2017-11-14 14:02:10 -0600

    Solucioncilla

---
+++ Triples/solution/100_B.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int n;
+vector<ll>sol;
+ll v[100002];
+	int main (){
+		ios_base::sync_with_stdio(0);
+		cin.tie(0);
+		cin>>n;
+		for(int i=0;i<n;i++){
+			cin>>v[i];
+		}
+		for(int i=0;i<n;i++){
+			if(v[i]%3==0)sol.push_back(i+1);
+		}
+		if(!(int)sol.size()){
+			cout << "no hay triples\n";
+			return 0;
+		}
+		cout << (int)sol.size()<<"\n";
+		for(int i=0;i<(int)sol.size();i++)cout << sol[i]<<" ";
+		cout << "\n";
+	}
