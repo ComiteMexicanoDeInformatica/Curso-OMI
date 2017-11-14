398e167c2b23aaf872cf323a7449dd1210f91072
troyodk <troyodk@github.com>
2017-11-14 13:31:17 -0600

    solucion

---
+++ ColaInteractivo/solution/solution.cpp
@@ -0,0 +1,20 @@
+﻿#include "ColaInteractivo.h"
+
+int arreglo[10000];
+int ini,fin;
+
+void agrega(int n)
+{
+	arreglo[fin] = n;
+	fin++;
+}
+
+int saca()
+{
+	if (fin-ini == 0) {
+		return -1;
+	} else {
+	    ini++;
+		return arreglo[ini-1];
+	}
+}
