0a1affa3ddbe3cefe1d383bd88521335fc6af690
vdae2304 <vdae_2304@hotmail.com>
2017-11-15 16:42:05 -0600

    Solucion lista

--- Quicksort/Main.cpp
+++ Quicksort/Main.cpp
@@ -7,14 +7,20 @@ using namespace std;
 int n, v[MAXN];
 
 int ElementoEnPosicion(int i) {
-    if (i < 0 || i >= n)
+    if (i < 0 || i >= n) {
+        printf("Mandaste a llamar ElementoEnPosicion() para una posicion invalida.\n");
         return -1;
+    }
+    printf("El elemento en la posicion %d es %d\n", i, v[i]);
     return v[i];
 }
 
 int Intercambiar(int i, int j) {
-    if (i < 0 || i >= n || j < 0 || j >= n)
+    if (i < 0 || i >= n || j < 0 || j >= n) {
+        printf("Mandaste a llamar Intercambiar() para posiciones invalidas.\n");
         return 0;
+    }
+    printf("Llamaste a intercambiar %d y %d\n", v[i], v[j]);
     int aux = v[i];
     v[i] = v[j];
     v[j] = aux;
@@ -23,16 +29,23 @@ int Intercambiar(int i, int j) {
 
 void Quicksort(int nsize, int arr[]) {
     if (nsize > 1) {
+        printf("Se mando a llamar HacerParticion() para el arreglo:\n");
+        for (int i = 0; i < nsize; i++)
+            printf("%d ", arr[i]);
+        printf("\n");
+
         n = nsize;
         memcpy(v, arr, sizeof(v));
         int piv = HacerParticion(nsize);
 
         int izq[nsize], der[nsize];
-        memcpy(izq, arr, sizeof(izq));
-        memcpy(der, arr + piv + 1, sizeof(der));
+        memcpy(izq, v, sizeof(izq));
+        memcpy(der, v + piv + 1, sizeof(der));
 
         Quicksort(piv, izq);
         Quicksort(nsize - piv - 1, der);
+        memcpy(arr, izq, sizeof(izq));
+        memcpy(arr + piv + 1, der, sizeof(der));
     }
 }
 
@@ -45,9 +58,9 @@ int main() {
 
     for (int i = 0; i < nsize - 1; i++)
         if (arr[i] > arr[i + 1]) {
-            printf("NO");
+            printf("No ordenaste el arreglo. :(\n");
             return 0;
         }
-    printf("SI");
+    printf("Ordenaste correctamente el arreglo. :)\n");
     return 0;
 }
