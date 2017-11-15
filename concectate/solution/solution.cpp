50cb132b6a46e992f133b2423626f9e3b8b5bcd5
Alex Ortiz <holaalex2204@hotmail.com>
2017-11-14 19:27:47 -0600

    Agrega Intercambios

---
+++ concectate/solution/solution.cpp
@@ -0,0 +1,106 @@
+#include <iostream>       // std::cout
+#include <stack>          // std::stack
+using namespace std;
+
+const int maxN = 100000;
+stack<int> arriba;
+stack<int> abajo;
+int placa[(2*maxN)]; 
+int salida[(2*maxN)]; 
+int siguiente[(2+maxN)]; 
+int n;
+int push(int num)
+{
+    int result = 0;
+    if(arriba.empty())
+    {
+        arriba.push(num);
+        result = 1;
+    }
+    else if(arriba.top()>num)
+    {
+        arriba.push(num);
+        result = 1;
+    }
+    else if(abajo.empty())
+    {
+        abajo.push(num);
+        result = 2;
+    }
+    else if(abajo.top()>num)
+    {
+        abajo.push(num);
+        result = 2;
+    }
+    else
+    {
+        result = -1;
+    }
+    return result;
+}
+int pop(int num)
+{
+    int result = 0;
+    if(!arriba.empty())
+    {
+        if(arriba.top()==num)
+        {
+            arriba.pop();
+            result = 1;
+        }
+    }
+    if(!abajo.empty())
+    {
+        if(abajo.top()==num)
+        {
+            abajo.pop();
+            result = 2;
+        }
+    }
+    return result;  
+}
+int main()
+{
+    int res  = 0;
+    bool esValido = true;
+    //lee el valor de N
+    cin >> n;
+    n = 2*n;
+    //Lee el orden de los elementos en la placa
+    for(int i = 0; i<n ; i++)
+    {
+        cin >> placa[i];
+        siguiente[placa[i]]= i;
+    }
+    for(int i = 0; i<n ; i++)
+    {
+        //Quiero saber si es el número más cercano a la derecha
+        if(siguiente[placa[i]]== i)
+        {
+            res = pop(i);
+            salida[i] = res;
+        }
+        else
+        {
+             res = push(siguiente[placa[i]]);
+            salida[i] = res;
+            if(res == -1)
+            {
+                esValido = false;
+                break;
+            }
+        }
+    }
+    if(esValido)
+    {
+        for(int i = 0; i<n ; i++)
+        {
+            cout << salida[i] << endl;
+        }
+    }
+    else
+    {
+        cout << 0 << endl;
+    }
+}
+
