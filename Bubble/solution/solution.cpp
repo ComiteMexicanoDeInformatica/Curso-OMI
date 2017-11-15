3f9ecccd8008f809da411c9bc5404aa9142dbc63
troyodk <troyodk@github.com>
2017-11-15 11:07:01 -0600

    Revert "TLE2"
    This reverts commit d5ae8d6c50306b3d6b77472243d67a062d8c0c72.

--- Bubble/solution/solution.cpp
+++ Bubble/solution/solution.cpp
@@ -5,7 +5,7 @@ int inicio(int n)
 {
 	for(int i=0;i<n;i++)
     {
-        for(int j=0;j<n-1-i;j++)
+        for(int j=0;j<n-1;j++)
         {
             if(pregunta(j)>pregunta(j+1))
             {
