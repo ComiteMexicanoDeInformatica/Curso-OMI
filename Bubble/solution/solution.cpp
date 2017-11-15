61d394ca3a8cd8d6bb6825f201eec7c8bf572de8
troyodk <troyodk@github.com>
2017-11-14 18:14:26 -0600

    solucion cosa

---
+++ Bubble/solution/solution.cpp
@@ -0,0 +1,17 @@
+﻿#include "Bubble.h"
+
+
+void inicio(int n)
+{
+	for(int i=0;i<n;i++)
+    {
+        for(int j=0,j<n-1;j++)
+        {
+            if(pregunta(j)>pregunta(j+1))
+            {
+                cambia(j);
+            }
+        }
+    }
+}
+
