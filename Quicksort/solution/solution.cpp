ee4333b97176526663143dc1f5e2ad3947cb149b
vdae2304 <vdae_2304@hotmail.com>
2017-11-16 16:39:19 -0600

    Se bajaron los limites

--- Quicksort/solution/solution.cpp
+++ Quicksort/solution/solution.cpp
@@ -1,20 +1,18 @@
 #include "quicksort.h"
 
 // int ElementoEnPosicion(int i);
-// int Intercambiar(int i, int j);
+// bool Intercambiar(int i, int j);
 
 int HacerParticion(int n) {
     int piv = ElementoEnPosicion(n - 1);
-    int i = -1, j;
+    int i = -1, j, k;
 
     for (j = 0; j < n - 1; j++)
         if (ElementoEnPosicion(j) < piv)
             Intercambiar(++i, j);
-    Intercambiar(++i, n - 1);
 
-    piv = ElementoEnPosicion(i);
-    for (j = i; j < n - 1; j++)
-        if (piv != ElementoEnPosicion(j + 1))
-            break;
-    return (i + j) / 2;
+    for (j = ++i, k = i; j < n; j++)
+        if (ElementoEnPosicion(j) == piv)
+            Intercambiar(++k, j);
+    return (++i + --k) / 2;
 }
