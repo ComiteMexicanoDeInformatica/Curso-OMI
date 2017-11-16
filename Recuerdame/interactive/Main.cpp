450b0d1a4dd06785a06d432a4f15791b6c00ed0c
Alexis Cervantes <pacharrin@gmail.com>
2017-11-16 12:08:49 -0600

    recuerdame commit inicial

---
+++ Recuerdame/interactive/Main.cpp
@@ -0,0 +1,32 @@
+#include "recuerdame.h"
+#include <algorithm>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numero;
+    int arr[100];
+    int preguntas;
+    int semilla;
+
+
+    cin >> numero >> semilla;
+
+    srand(semilla);
+
+    recuerdame(numero);
+
+    preguntas = rand() % 99 + 2;
+
+    bool correcto = true;
+
+    for (int i = 0; i < preguntas; i++) {
+        int x = rand() % 10 +1;
+        correcto &= (soy(x) == (x == numero));
+    }
+
+    cout << correcto <<endl;
+}
