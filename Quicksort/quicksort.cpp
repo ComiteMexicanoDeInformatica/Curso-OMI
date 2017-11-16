e5e795406c361d42ab8940cb7b5c58d2dacf4942
vdae2304 <vdae_2304@hotmail.com>
2017-11-16 17:49:53 -0600

    Ahí tienen papus

--- Quicksort/quicksort.cpp
+++ Quicksort/quicksort.cpp
@@ -1,9 +1,19 @@
 #include "quicksort.h"
 
 // int ElementoEnPosicion(int i);
-// int Intercambiar(int i, int j);
+// bool Intercambiar(int i, int j);
 
-int HacerParticion(int n) {
-    //FIXME
-    return 0;
+int HacerParticion(int inicio, int fin) {
+    int piv = ElementoEnPosicion(fin - 1);
+    int i = inicio - 1, j = fin, k;
+
+    for (k = inicio; k < fin; k++)
+        if (ElementoEnPosicion(k) < piv)
+            Intercambiar(++i, k);
+
+    for (k = fin - 1; k >= inicio; k--)
+        if (ElementoEnPosicion(k) > piv)
+            Intercambiar(--j, k);
+
+    return (i + j) / 2;
 }
