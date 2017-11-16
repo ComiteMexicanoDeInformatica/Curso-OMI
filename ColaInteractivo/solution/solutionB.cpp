d25e8cc36c24664891a7340de2811dca7141d76d
ErickB <erick.jassi.bs@gmail.com>
2017-11-16 13:57:08 -0600

    segunda solucion colainteractivo

---
+++ ColaInteractivo/solution/solutionB.cpp
@@ -0,0 +1,17 @@
+#include "ColaInteractivo.h"
+
+int queue[10000];
+int start = 0, end = 0;
+
+int queue[10000];
+int start = 0, end = 0;
+
+void agrega(int N) {
+    queue[end++] = N;
+}
+
+int saca() {
+    if(start == end)
+        return -1;
+    return queue[start++];
+}
