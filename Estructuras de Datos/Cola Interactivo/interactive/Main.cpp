75509015af37d0582f190660ea97a411d8299ae5
Freddy Román <me@freddy.mx>
2017-11-17 13:36:00 -0600

    Cola Interactivo.

---
+++ Estructuras de Datos/Cola Interactivo/interactive/Main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include"ColaInteractivo.h"
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 0; i< n; i++) {
+        char a;
+        cin >> a;
+
+        if (a == 'a') {
+            int value;
+            cin >> value;
+
+            agrega(value);
+        } else if (a == 's') {
+            cout << saca() << endl;
+        }
+    }
+}
