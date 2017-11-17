b1c537ad105b276be4c8309c458f384a3abb63d9
José Soto <jaas9801@gmail.com>
2017-11-16 19:21:19 -0600

    dcs
    asdg

--- DeCuartoEnCuarto/solution/DeCuartoEnCuarto.cpp
+++ DeCuartoEnCuarto/solution/DeCuartoEnCuarto.cpp
@@ -12,13 +12,13 @@ void Player1(int x,int y){
 
     int v1 = siguienteCuarto(1), v2 = siguienteCuarto(2);
 
-    if(v1!=-1 && !use[v1] && !done){
+    if(!use[v1] && !done){
         move(v1);
         Player1(v1,y);
         if(!done) regresa();
     }
 
-    if(v2!=-1 && !use[v2] && !done){
+    if(!use[v2] && !done){
         move(v2);
         Player1(v2,y);
         if(!done) regresa();
