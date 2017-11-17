210b04857cda4c0ce9d686c2b53b76c62f242944
Briaares <diego.gonzalez@cimat.mx>
2017-11-17 17:16:48 -0600

    achis achis los mariachis

---
+++ Quicksort/solution/100_B.cpp
@@ -0,0 +1,15 @@
+#include "quicksort.h"
+
+int HacerParticion(int inicio, int fin) {
+    int piv = ElementoEnPosicion(fin - 1);
+    int i = inicio-1,j=fin;
+    for (int k = inicio; k < fin; k++)
+        if (ElementoEnPosicion(k) < piv)
+            Intercambiar(++i, k);
+
+    for (int k = fin - 1; k >= inicio; k--)
+        if (ElementoEnPosicion(k) > piv)
+            Intercambiar(--j, k);
+
+    return (i + j) / 2;
+}
