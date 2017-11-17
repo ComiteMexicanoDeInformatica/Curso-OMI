817ab65daff7616e731c375ce095cea912c7da51
ErickB <erick.jassi.bs@gmail.com>
2017-11-17 11:33:13 -0600

    Pacman interactive

---
+++ Pacman/interactive/Main.cpp
@@ -0,0 +1,18 @@
+#include "Pacman.h"
+#include <iostream>
+
+using namespace std;
+
+int F, C, P, R;
+
+int hayComida(int pos){
+    return 0;
+}
+
+int esPosibleAvanzar(int posActual, char direccion){
+    return 0;
+}
+
+int main() {
+
+}
