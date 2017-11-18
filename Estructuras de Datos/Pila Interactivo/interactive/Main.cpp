e9b899816a1a7d76b61f387ba87cd807740cbc22
vdae2304 <vdae_2304@hotmail.com>
2017-11-18 11:46:52 -0600

    A ver si ya jala

--- Estructuras de Datos/Pila Interactivo/interactive/Main.cpp
+++ Estructuras de Datos/Pila Interactivo/interactive/Main.cpp
@@ -1,7 +1,7 @@
 #include "PilaInteractivo.h"
 #include<stdio.h>
 
-int n, val;
+int n, val, aux;
 char ins;
 
 int main() {
@@ -9,13 +9,18 @@ int main() {
 
     for(int i = 0; i < n; i++){
         scanf("%c", &ins);
+        aux = 0;
         if (ins == 'a') {
             scanf("%d", &val);
             agrega(val);
         }
-        if (ins == 's')
+        if (ins == 's') {
             printf("%d\n", saca());
+            aux = 1;
+        }
     }
-    printf("\n");
+
+    if (aux == 0)
+        printf("\n");
     return 0;
 }
