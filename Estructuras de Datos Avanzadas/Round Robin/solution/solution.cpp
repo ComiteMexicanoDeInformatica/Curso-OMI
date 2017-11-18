81019ad913085ff446fbf5f904890b31602a33b3
vdae2304 <vdae_2304@hotmail.com>
2017-11-18 11:05:34 -0600

    Para que se vea más fancy

--- Estructuras de Datos Avanzadas/Round Robin/solution/solution.cpp
+++ Estructuras de Datos Avanzadas/Round Robin/solution/solution.cpp
@@ -1,40 +1,34 @@
-#include<iostream>
-#include<stdio.h>
-#include<queue>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <stdio.h>
+#include <queue>
 using namespace std;
 
-struct tar
-{
-    int a,b;
+struct tar {
+    int a, b;
 };
 queue<tar> troy;
+
 int main()
 {
     ios_base::sync_with_stdio(0);
-    int a,b;
-cin>>a>>b;
-tar c;
-for(int i=0;i<a;i++)
-{
+    int a, b;
+    cin >> a >> b;
+    tar c;
 
-    cin>>c.a>>c.b;
-    troy.push(c);
-}
-while(!troy.empty())
-{
-    c=troy.front();
-    c.b-=b;
-    if(c.b<=0)
-    {
-        cout<<c.a<<endl;
-    }
-    else
-    {
+    for(int i = 0; i < a; i++) {
+        cin >> c.a >> c.b;
         troy.push(c);
     }
-    troy.pop();
-}
 
-
+    while(!troy.empty()) {
+        c = troy.front();
+        c.b -= b;
+        if(c.b <= 0) {
+            cout << c.a << endl;
+        }
+        else {
+            troy.push(c);
+        }
+        troy.pop();
+    }
 }
