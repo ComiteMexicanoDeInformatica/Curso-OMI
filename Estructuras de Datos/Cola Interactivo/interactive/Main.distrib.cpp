e35053e4a2e234088764c59cd151faa7f8368012
troyodk <troyodk@github.com>
2017-11-18 13:37:58 -0600

    lol

---
+++ Estructuras de Datos/Cola Interactivo/interactive/Main.distrib.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <iomanip>
+
+#include "ColaInteractivo.h"
+
+using namespace std;
+
+int main()
+{
+    cin.sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    cerr << n << endl;
+int r=0;
+    for (int i = 0; i < n; i++) {
+        char a;
+        cin >> a;
+
+        if (a == 'a') {
+            int value;
+            cin >> value;
+
+            agrega(value);
+
+        } else if (a == 's') {
+            cout<<saca()<<endl
+        }
+    }
+   // cout<<r;
+}
