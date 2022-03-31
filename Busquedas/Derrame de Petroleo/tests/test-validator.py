dfc54530f226e0c77ab8d1aad5170d92f58f4d0a
Freddy Román <me@freddy.mx>
2022-03-31 10:13:42 -0700

    Petróleo (#20)
    * fun
    
    * fun2
    
    * cleanup
    
    * Update test-validator.py
    
    * Update generator.cpp
    
    * Update test-validator.py
    
    * Update problems.json

---
+++ Busquedas/Derrame de Petroleo/tests/test-validator.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# no-self-use
+# pylint: disable=R0201
+
+import sys
+import re
+import unittest
+
+from omegaup.validator import validatortest
+
+
+class Test(unittest.TestCase):
+
+    def test(self):
+        with open('data.in', 'r') as handle:
+            lines = handle.read().split('\n')
+
+        self.assertEqual(lines[-1], '', "file doesn't end with EOL")
+        lines.pop()
+
+        first_line_reg = re.compile(r'^(\d+) (\d+) (\d+)$')
+        self.assertTrue(first_line_reg.match(lines[0]),
+                        "first line incorrectly formatted")
+        R, C, K = map(int, lines[0].split(' '))
+
+        # Validar R, C, K
+        self.assertTrue(1 <= R <= 2000, "R out of bounds")
+        self.assertTrue(1 <= C <= 2000, "C out of bounds")
+        self.assertTrue(0 <= K <= 10**7, "K out of bounds")
+
+        # Validar número de lineas
+        self.assertEqual(len(lines), 1 + R, "line count mismatch")
+
+        saw_dollar = False
+        map_reg = re.compile(r'^[.#$]+$')
+        for line in lines[1:]:
+            self.assertTrue(map_reg.match(line), "invalid character in map")
+            self.assertEqual(len(line), C, "line width mismatch")
+            if '$' in line:
+                self.assertFalse(saw_dollar, "too many $ in map")
+
+        self.assertFalse(saw_dollar, "no $ in map")
+
+
+if __name__ == '__main__':
+    validatortest.main()
